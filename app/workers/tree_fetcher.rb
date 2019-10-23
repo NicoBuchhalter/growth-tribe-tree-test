d7d124f5d49bed5c1bb9fc973e81ed92c07a7b9e
NicoBuchhalter <nico.buch.1010@gmail.com>
2019-10-23 11:33:59 +0200

    Delete old tree when fetching new

--- app/workers/tree_fetcher.rb
+++ app/workers/tree_fetcher.rb
@@ -6,7 +6,10 @@ class TreeFetcher
 
   base_uri Rails.application.credentials.tree_fetcher_url
 
+  # Every requested tree comes with ID 1.
+  # So the only way to fetch other tree without repeated IDs is to delete them all.
   def perform
+    TreeNode.delete_all
   	random_tree = JSON.parse(self.class.get('/').body)
   	root = TreeNode.create!(external_id: random_tree['id'])
   	create_children(root, random_tree['child'])
